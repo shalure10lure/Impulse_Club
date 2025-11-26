20dcf80724ebb5548b685c65dce646cfa0007625
shalure10lure <yessicarado@gmail.com>
2025-11-25 21:45:20 -0400

    feat: adding required in entity user

--- Models/User.cs
+++ Models/User.cs
@@ -5,10 +5,10 @@ namespace ImpulseClub.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; } = "User";//"User" | "Admin"
+        [Required] public string Username { get; set; }
+        [Required] public string Email { get; set; }
+        [Required] public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin" | "Fundador" 
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiresAt { get; set; }
         public DateTime? RefreshTokenRevokedAt { get; set; }
