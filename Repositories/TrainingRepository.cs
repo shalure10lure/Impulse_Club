c4c26b141d641457824e144bb8e15547bc5a8041
shalure10lure <yessicarado@gmail.com>
2025-11-27 21:09:10 -0400

    feat:addign training repository and itrainingrepository

---
+++ Repositories/TrainingRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using ImpulseClub.Data;
+using ImpulseClub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImpulseClub.Repositories
+{
+    public class TrainingRepository : ITrainingRepository
+    {
+        private readonly AppDbContext _db;
+        public TrainingRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Add(Training training)
+        {
+            await _db.Trainings.AddAsync(training);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Training training)
+        {
+            _db.Trainings.Remove(training);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Training>> GetAll()
+        {
+            return await _db.Trainings.ToListAsync();
+        }
+
+        public async Task<Training> GetOne(Guid id)
+        {
+            return await _db.Trainings.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(Training training)
+        {
+            _db.Trainings.Update(training);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
