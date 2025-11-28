c1f35bf6cab4962136a111ff748aa9eba197a124
shalure10lure <yessicarado@gmail.com>
2025-11-27 20:17:55 -0400

    feat:addign club repository and iclub repository

---
+++ Repositories/ClubRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using ImpulseClub.Data;
+using ImpulseClub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImpulseClub.Repositories
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly AppDbContext _db;
+        public ClubRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Add(Club club)
+        {
+            await _db.Clubes.AddAsync(club);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Club club)
+        {
+            _db.Clubes.Remove(club);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            return await _db.Clubes.ToListAsync();
+        }
+
+        public async Task<Club> GetOne(Guid id)
+        {
+            return await _db.Clubes.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(Club club)
+        {
+            _db.Clubes.Update(club);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
