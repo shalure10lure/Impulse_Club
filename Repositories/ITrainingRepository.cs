ccf7811abb0f05d78ec2400fdc192477dff55650
leitocam <leonardo.camacho@ucb.edu.bo>
2025-11-27 22:26:59 -0400

    feat: implement TrainingRepository with EF Core queries

--- Repositories/ITrainingRepository.cs
+++ Repositories/ITrainingRepository.cs
@@ -1,11 +1,20 @@
-﻿namespace ImpulseClub.Repositories
+﻿using ImpulseClub.Entities;
+
+namespace ImpulseClub.Repositories
 {
     public interface ITrainingRepository
     {
         Task<IEnumerable<Training>> GetAll();
-        Task<Training> GetOne(Guid id);
+        Task<IEnumerable<Training>> GetAllAsync();
+        Task<Training?> GetOne(Guid id);
+        Task<Training?> GetByIdAsync(Guid id);
+        Task<Training?> GetByIdWithParticipantsAsync(Guid id);
+        Task<IEnumerable<Training>> GetByClubIdAsync(Guid clubId);
         Task Add(Training training);
+        Task AddAsync(Training training);
         Task Update(Training training);
+        Task UpdateAsync(Training training);
         Task Delete(Training training);
+        Task DeleteAsync(Training training);
     }
 }
--- Repositories/TrainingRepository.cs
+++ Repositories/TrainingRepository.cs
@@ -1,43 +1,92 @@
-﻿
-using ImpulseClub.Data;
-using ImpulseClub.Models;
+﻿using ImpulseClub.Data;
+using ImpulseClub.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ImpulseClub.Repositories
 {
     public class TrainingRepository : ITrainingRepository
     {
-        private readonly AppDbContext _db;
-        public TrainingRepository(AppDbContext db)
-        {
-            _db = db;
-        }
-        public async Task Add(Training training)
-        {
-            await _db.Trainings.AddAsync(training);
-            await _db.SaveChangesAsync();
-        }
+        private readonly AppDbContext _context;
 
-        public async Task Delete(Training training)
+        public TrainingRepository(AppDbContext context)
         {
-            _db.Trainings.Remove(training);
-            await _db.SaveChangesAsync();
+            _context = context;
         }
 
         public async Task<IEnumerable<Training>> GetAll()
         {
-            return await _db.Trainings.ToListAsync();
+            return await _context.Trainings.Include(t => t.Club).ToListAsync();
         }
 
-        public async Task<Training> GetOne(Guid id)
+        public async Task<IEnumerable<Training>> GetAllAsync()
         {
-            return await _db.Trainings.FirstOrDefaultAsync(x => x.Id == id);
+            return await _context.Trainings
+                .Include(t => t.Club)
+                .ToListAsync();
+        }
+
+        public async Task<Training?> GetOne(Guid id)
+        {
+            return await _context.Trainings.Include(t => t.Club).FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<Training?> GetByIdAsync(Guid id)
+        {
+            return await _context.Trainings
+                .Include(t => t.Club)
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<Training?> GetByIdWithParticipantsAsync(Guid id)
+        {
+            return await _context.Trainings
+                .Include(t => t.Participants)
+                .Include(t => t.Club)
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<Training>> GetByClubIdAsync(Guid clubId)
+        {
+            return await _context.Trainings
+                .Where(t => t.ClubId == clubId)
+                .Include(t => t.Club)
+                .ToListAsync();
+        }
+
+        public async Task Add(Training training)
+        {
+            _context.Trainings.Add(training);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddAsync(Training training)
+        {
+            _context.Trainings.Add(training);
+            await _context.SaveChangesAsync();
         }
 
         public async Task Update(Training training)
         {
-            _db.Trainings.Update(training);
-            await _db.SaveChangesAsync();
+            _context.Trainings.Update(training);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Training training)
+        {
+            _context.Trainings.Update(training);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Training training)
+        {
+            _context.Trainings.Remove(training);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Training training)
+        {
+            _context.Trainings.Remove(training);
+            await _context.SaveChangesAsync();
         }
     }
 }
