f0e8ba3e68a11d2e9b553ed5671c3459d30ac298
leitocam <leonardo.camacho@ucb.edu.bo>
2025-11-27 22:24:53 -0400

    feat: add Training DTOs for API requests and responses

---
+++ Models/DTOS/UpdateTrainingDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ImpulseClub.Models.DTOS
+{
+    public class UpdateTrainingDto
+    {
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        [Range(1, 480)]
+        public int? Duration { get; set; }
+
+        public string? UsedResources { get; set; } // JSON format
+    }
+}
